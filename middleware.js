4368df73315495c50a3a91d99f44a95922a11507
Andri Febrian <andri.feb.26@gmail.com>
2025-07-21 10:32:29 +0700

    A

--- middleware.js
+++ middleware.js
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const pathname = request.nextUrl.pathname;
+  const { pathname } = request.nextUrl;
 
-  if (pathname === "/sitemap.xml" || pathname === "/sitemap-0.xml") {
+  if (pathname.startsWith("/sitemap") && pathname.endsWith(".xml")) {
     const response = NextResponse.next();
 
+    // Hapus semua header yang mengganggu indexing
     response.headers.set("Cache-Control", "public, max-age=0, must-revalidate");
     response.headers.delete("Pragma");
     response.headers.delete("Expires");
